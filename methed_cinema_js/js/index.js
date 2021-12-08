4210937ef9f84d2c2e97460b5265bd33a3345fc6
Vova <vovik142@ukr.net>
2021-12-08 10:01:01 +0200

    add menu

---
+++ methed_cinema_js/js/index.js
@@ -0,0 +1,11 @@
+import slideMenu from "./menu.js";
+
+slideMenu({
+    openBtn: '.header__burger-btn',
+    menu: '.navigation',
+    classActiveMenu: 'navigation_active',
+    closeTrigger: '.navigation__link, .navigation__close',
+});
+
+
+
---
+++ methed_cinema_js/js/menu.js
@@ -0,0 +1,29 @@
+
+const openMenu = (nav, active) => {
+    nav.classList.add(active);
+}
+
+const closeMenu = (nav, active) => {
+    nav.classList.remove(active)
+
+}
+
+
+const slideMenu = ({openBtn, menu, classActiveMenu, closeTrigger}) => {
+    const burgerBtn = document.querySelector(openBtn)
+    const navigation = document.querySelector(menu)
+    const navigationClose = document.querySelectorAll(closeTrigger)
+    console.log (navigationClose)
+    
+    burgerBtn.addEventListener('click', () => {
+        openMenu(navigation, classActiveMenu)
+    });
+
+    navigationClose.forEach(item => {
+        item.addEventListener('click', () => {
+            closeMenu(navigation, classActiveMenu)
+        })
+    })
+};
+
+export default slideMenu;
